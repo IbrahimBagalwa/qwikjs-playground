55d8c33e8dfc458a81dd03c3f657462709f7ed2e
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2024-04-21 01:13:25 +0200

    feat: creating card contact and fetch data

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,10 +1,46 @@
-import { component$ } from '@builder.io/qwik'
+import { Resource, component$, useResource$ } from '@builder.io/qwik'
 
+import { CONTACTS, Contact } from './fake-contact-db'
+import styles from './contat.module.css'
 export default component$(() => {
+  const contactsResponse = useResource$<Contact[]>(async () => {
+    return await Promise.resolve(CONTACTS)
+  })
   return (
     <div>
       <h1>Contact</h1>
-      <p>This is the contact page</p>
+      <Resource
+        value={contactsResponse}
+        onPending={() => <div>Loading...</div>}
+        onResolved={(contact) => {
+          return (
+            <section class={styles.container}>
+              {contact.map((contact) => (
+                <div key={contact.id} class={styles.content}>
+                  <img
+                    width='200'
+                    height='200'
+                    src={contact.avatar}
+                    alt={contact.name}
+                    class={styles.avatar}
+                  />
+                  <div>
+                    <p class={styles.name}>{contact.name}</p>
+                    <p class={styles.role}>{contact.role}</p>
+                    <div class={styles.link}>
+                      <a href={contact.twitter}>twitter</a>
+                      <span> | </span>
+                      <a href={contact.linkedin}>linkedin</a>
+                      <span> | </span>
+                      <a href={contact.github}>github</a>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </section>
+          )
+        }}
+      />
     </div>
   )
 })
