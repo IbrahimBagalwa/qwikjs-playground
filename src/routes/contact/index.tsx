9e0777fac76b46cec0b278fe1ac987aa004d2484
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2024-04-21 02:04:46 +0200

    refactor: optimize the search

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -22,37 +22,38 @@ export default component$(() => {
         value={contactsResponse}
         onPending={() => <div>Loading...</div>}
         onResolved={(contacts) => {
+          const filterContact = contacts.filter(
+            (c) =>
+              c.name
+                .toLowerCase()
+                .indexOf(filteredContacts.value.toLowerCase()) > -1
+          )
+          const mappedContact =
+            filterContact.length > 0 ? filterContact : contacts
           return (
             <section class={styles.container}>
-              {contacts
-                .filter(
-                  (c) =>
-                    c.name
-                      .toLowerCase()
-                      .indexOf(filteredContacts.value.toLowerCase()) > -1
-                )
-                .map((contact) => (
-                  <div key={contact.id} class={styles.content}>
-                    <img
-                      width='200'
-                      height='200'
-                      src={contact.avatar}
-                      alt={contact.name}
-                      class={styles.avatar}
-                    />
-                    <div>
-                      <p class={styles.name}>{contact.name}</p>
-                      <p class={styles.role}>{contact.role}</p>
-                      <div class={styles.link}>
-                        <a href={contact.twitter}>twitter</a>
-                        <span> | </span>
-                        <a href={contact.linkedin}>linkedin</a>
-                        <span> | </span>
-                        <a href={contact.github}>github</a>
-                      </div>
+              {mappedContact.map((contact) => (
+                <div key={contact.id} class={styles.content}>
+                  <img
+                    width='200'
+                    height='200'
+                    src={contact.avatar}
+                    alt={contact.name}
+                    class={styles.avatar}
+                  />
+                  <div>
+                    <p class={styles.name}>{contact.name}</p>
+                    <p class={styles.role}>{contact.role}</p>
+                    <div class={styles.link}>
+                      <a href={contact.twitter}>twitter</a>
+                      <span> | </span>
+                      <a href={contact.linkedin}>linkedin</a>
+                      <span> | </span>
+                      <a href={contact.github}>github</a>
                     </div>
                   </div>
-                ))}
+                </div>
+              ))}
             </section>
           )
         }}
